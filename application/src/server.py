e0cf08b3b4e2d95cc84771d6945a9baa4332a8bd
vitriolfox <vitriol.fox@gmail.com>
2025-09-24 04:54:25 +0200

    Update server.py

--- application/src/server.py
+++ application/src/server.py
@@ -6,6 +6,7 @@ import database
 app = Flask(__name__)
 
 @app.route('/book', methods=['GET'])
+
 def get_books():
 
     books = database.book_database
@@ -13,4 +14,6 @@ def get_books():
     return jsonify(books)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=8080)
+    
+    app.run(debug=True, host='0.0.0.0', port=8080)
+    
